a680c2f84504464e41d164c413561e31347fa164
CDemmenie <11903978+Demmenie@users.noreply.github.com>
2025-01-02 16:45:49 +0100

    Rating script

---
+++ rating.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = pd.read_csv("data/annotation.csv")
+data = data.astype(str)
+
+anno = input("Annotator 1 or 2? ")
+anno = "label" + str(anno)
+
+for index in range(len(data)):
+
+    summary = data.loc[index, "summary"]
+    print(summary)
+
+    label = input("Is this note biased (1) or unbiased (0)? ")
+
+    if label in ["0", "1"]:
+        data.loc[index, anno] = str(label)
+
+        data.to_csv("data/annotation.csv",
+                    columns=["noteId", "status", "summary", "label1", "label2"])
